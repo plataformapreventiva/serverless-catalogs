8a964d27d48a39b43a8cf038b781b8bfb0145785
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-07-22 23:29:22 +0000

    pub re order

--- pub/pyspark/run_pub.sh
+++ pub/pyspark/run_pub.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 spark-submit --conf spark.executor.memory=7g --conf spark.executor.instances=8 --conf spark.executor.cores=5 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/usr/bin/python3 --conf spark.executorEnv.PYSPARK_PYTHON=/usr/bin/python3 pub-clean-new2.py
 #spark-submit pub-clean-new2.py
